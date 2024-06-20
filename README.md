ada81b33cf0533a76b17d2df6d29e512038fbb8f
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-06-20 10:31:26 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Populate Labelbox Workspace
+
+These scripts are designed to populate Labelbox Workspaces with public data for testing purposes.
+
+## Regular Data Rows
+
+The main script is `import.py` run this script and it will parse through the text files in the `files` folder. These files contain gs urls to public available assets. The name of the text files are the name of your Labelbox Dataset with the "_" replaces with a space.
+
+## Geospatial Data Rows
+
+The `import_tile.py` script is uses to import tile data. It does the same thing as the `import.py` script but use the `files_tile` folder.
---
+++ import_tile.py
@@ -0,0 +1,27 @@
+import labelbox as lb
+import os
+
+API_KEY = None
+
+data_rows = []
+
+for file_name in os.listdir("./files"):
+    with open(f"./files_tile/{file_name}", "r") as file:
+        for line in file.readlines():
+            if not line:
+                continue
+            file_path = line[5:].rstrip()
+            full_file = "https://storage.googleapis.com/" + file_path
+            data_rows.append({
+                "row_data": {"tileLayerUrl": full_file},
+                "global_key": file_path,
+            })
+
+    client = lb.Client(API_KEY)
+
+    dataset = client.create_dataset(name=file_name.replace("_", " ").rstrip(".txt"), iam_integration=None)
+
+    task = dataset.create_data_rows(data_rows)
+    task.wait_till_done()
+
+    print(task.errors)
