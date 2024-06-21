d7edeb6127625d7521786a90dfefcc71695f9f6a
Gabefire <33893811+Gabefire@users.noreply.github.com>
2024-06-21 09:19:27 -0500

    removed api key

--- import_tile.py
+++ import_tile.py
@@ -1,7 +1,7 @@
 import labelbox as lb
 import os
 
-API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjazY3M25vd2E1ZWd3MDcwMGowYXFod3RxIiwib3JnYW5pemF0aW9uSWQiOiJjazY3M25vdTV4bTUxMDg0MnI2YWJsMWNjIiwiYXBpS2V5SWQiOiJjbHhvcnl2YzMwMGkwMDd6OTAwcm03cWRoIiwic2VjcmV0IjoiZmFjN2YyYWQyMTI3Zjk5ZDJkZmNjZDlmNzY5NzIwMDIiLCJpYXQiOjE3MTg5Nzk0MjcsImV4cCI6MjM1MDEzMTQyN30.xxfirYaQAg9JBM5Rj4bkEav9Uh47CAoAqT5WmxM-1iw"
+API_KEY = None
 
 data_rows = []
 
